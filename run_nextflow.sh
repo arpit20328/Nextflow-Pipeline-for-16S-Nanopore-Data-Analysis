7e47e88be30c9c646d8a15e822abafca72c20e70
ARPIT MATHUR <78298300+arpit20328@users.noreply.github.com>
2025-10-14 07:21:38 +0530

    Delete run_nextflow.sh

--- run_nextflow.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-nextflow -c /home/arpit/nextflow_16s/new_pipeline/nextflow.config run main.nf -entry NANOPORE_16S \
---sequences /home/arpit/nextflow_16s/new_pipeline/sequences/ \
---input /home/arpit/nextflow_16s/new_pipeline/samplesheet.csv \
--resume -bg
